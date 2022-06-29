89d8668d5b3b5d8dce50b2374e06b047d9511ef6
Kardast <dottorsf@gmail.com>
2022-06-29 16:42:01 +0200

    Layout

---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<header>
+
+    <img src="./assets/img/logo-small.svg" alt="spotify">
+</header>
--- includes/main-home.php
+++ includes/main-home.php
@@ -1,8 +1,35 @@
-<main><?php
+<main>
+    
+    <div class="disks-wrapper">
 
-foreach ($arr_disks as $n_disk) { ?>
-    <img src="<?php $n_disk['poster'] ?>" alt="<?php $n_disk['title'] ?>"> <?php
-    echo $n_disk['year'];
-} ?>
+        <?php
 
+        foreach ($arr_disks as $n_disk) { ?>
+            <!-- poster -->
+            <div class="disk-card">
+                <img 
+                    src="<?= $n_disk['poster'] ?>" 
+                    alt="<?php echo $n_disk['title'] ?>"
+                >
+                <!-- title -->
+                <h2>
+                    <?php echo $n_disk['title'] ?>
+                </h2>
+            
+                <!-- author -->
+                <p>
+                    <?php echo $n_disk['author'] ?>
+                </p>
+            
+                <!-- year -->
+                <p>
+                    <?php echo $n_disk['year'] ?>
+                </p>
+            </div> 
+
+            <?php
+        } ?>
+
+    </div>
+    
 </main>

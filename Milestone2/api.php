5c4effbbba7bd2618d59621490e3f48e44c8b492
Kardast <dottorsf@gmail.com>
2022-06-29 18:20:45 +0200

    Milestone 2

--- Milestone2/api.php
+++ Milestone2/api.php
@@ -1,8 +1,8 @@
 <?php
 
-include_once __DIR__ . '/database/data.php';
+include __DIR__ . '/database/data.php';
 
 header('Content-Type: application/json');
 
-echo json_encode('$arr_disks');
+echo json_encode($arr_disks);
 // includo il file contentente il mio array, specifico il tipo di header necessario, faccio l'echo dell'array trasformato in json tramite la funzione json-encode
